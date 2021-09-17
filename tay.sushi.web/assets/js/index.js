d26053026e7c98aeb56bdd4895e8bc892353f51f
dangphuongtay <2011437@dlu.edu.vn>
2021-09-17 09:15:18 +0700

    chan

--- tay.sushi.web/assets/js/index.js
+++ tay.sushi.web/assets/js/index.js
@@ -1,112 +1,87 @@
-    let menu = document.querySelector('#menu-bars');
-    let navbar = document.querySelector('.tabs');
+   let menu = document.querySelector('#menu-bars');
+   let navbar = document.querySelector('.tabs');
 
-    menu.onclick = () => {
-        menu.classList.toggle('close-outline');
-        navbar.classList.toggle('home-tabs');
-    }
+   menu.onclick = () => {
+       menu.classList.toggle('close-outline');
+       navbar.classList.toggle('home-tabs');
+   }
 
-    let section = document.querySelectorAll('section');
-    let navLinks = document.querySelectorAll('header .tabs a');
+   let section = document.querySelectorAll('section');
+   let navLinks = document.querySelectorAll('header .tabs a');
 
-    window.onscroll = () => {
+   window.onscroll = () => {
 
-        menu.classList.remove('close-outline');
-        navbar.classList.remove('home-tabs');
+       menu.classList.remove('close-outline');
+       navbar.classList.remove('home-tabs');
 
-        section.forEach(sec => {
+       section.forEach(sec => {
 
-            let top = window.scrollY;
-            let height = sec.offsetHeight;
-            let offset = sec.offsetTop - 150;
-            let id = sec.getAttribute('id');
+           let top = window.scrollY;
+           let height = sec.offsetHeight;
+           let offset = sec.offsetTop - 150;
+           let id = sec.getAttribute('id');
 
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(links => {
-                    links.classList.remove('home-tabs');
-                    document.querySelector('header .tabs a[href*=' + id + ']').classList.add('home-tabs');
-                });
-            };
+           if (top >= offset && top < offset + height) {
+               navLinks.forEach(links => {
+                   links.classList.remove('home-tabs');
+                   document.querySelector('header .tabs a[href*=' + id + ']').classList.add('home-tabs');
+               });
+           };
 
-        });
+       });
 
-    }
-    let menu = document.querySelector('#setting');
-    let navbar = document.querySelector('.tabs-setting');
+   }
+   let menu = document.querySelector('#setting');
+   let navbar = document.querySelector('.tabs-setting');
 
-    menu.onclick = () => {
-        menu.classList.toggle('close-outline');
-        navbar.classList.toggle('#setting');
-    }
+   menu.onclick = () => {
+       menu.classList.toggle('close-outline');
+       navbar.classList.toggle('#setting');
+   }
 
-    let section = document.querySelectorAll('section');
-    let tabsetting = document.querySelectorAll('header .tabs-setting .box');
+   var swiper = new Swiper(".homes", {
+       spaceBetween: 30,
+       centeredSlides: true,
+       autoplay: {
+           delay: 2000,
+           disableOnInteraction: false,
+       },
+       pagination: {
+           el: ".swiper-pagination",
+           clickable: true,
+       },
+       loop: true,
+   });
+   var swiper = new Swiper(".review-swiper", {
+       spaceBetween: 20,
+       centeredSlides: true,
+       autoplay: {
+           delay: 2500,
+           disableOnInteraction: false,
+       },
+       loop: true,
+       breakpoints: {
+           0: {
+               slidesPerView: 1,
+           },
+           640: {
+               slidesPerView: 2,
+           },
+           768: {
+               slidesPerView: 2,
+           },
+           1024: {
+               slidesPerView: 3,
+           },
+       },
+   });
 
-    window.onscroll = () => {
+   function loader() {
+       document.querySelector('.loader-container').classList.add('fade-out');
+   }
 
-        menu.classList.remove('close-outline');
-        navbar.classList.remove('setting');
+   function fadeOut() {
+       setInterval(loader, 1000);
+   }
 
-        section.forEach(sec => {
-
-            let top = window.scrollY;
-            let height = sec.offsetHeight;
-            let offset = sec.offsetTop - 150;
-            let id = sec.getAttribute('id');
-
-            if (top >= offset && top < offset + height) {
-                navLinks.forEach(links => {
-                    links.classList.remove('setting');
-                    document.querySelector('header .tabs-setting  a[href*=' + id + ']').classList.add('setting');
-                });
-            };
-
-        });
-
-    }
-    var swiper = new Swiper(".homes", {
-        spaceBetween: 30,
-        centeredSlides: true,
-        autoplay: {
-            delay: 2000,
-            disableOnInteraction: false,
-        },
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        loop: true,
-    });
-    var swiper = new Swiper(".review-swiper", {
-        spaceBetween: 20,
-        centeredSlides: true,
-        autoplay: {
-            delay: 2500,
-            disableOnInteraction: false,
-        },
-        loop: true,
-        breakpoints: {
-            0: {
-                slidesPerView: 1,
-            },
-            640: {
-                slidesPerView: 2,
-            },
-            768: {
-                slidesPerView: 2,
-            },
-            1024: {
-                slidesPerView: 3,
-            },
-        },
-    });
-
-    function loader() {
-        document.querySelector('.loader-container').classList.add('fade-out');
-    }
-
-    function fadeOut() {
-        setInterval(loader, 1000);
-    }
-
-    window.onload = fadeOut;
+   window.onload = fadeOut;
