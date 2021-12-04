e889098c84dc6ccb2b6ac1cb5086ced9038b892b
dangphuongtay <2011437@dlu.edu.vn>
2021-12-04 09:32:19 +0700

    push

--- web-cua-tui-6-nha/js/main.js
+++ web-cua-tui-6-nha/js/main.js
@@ -17,10 +17,19 @@ buttom.onclick = function () {
   //dữ liệu đầu ra
   document.getElementById("name-out").innerHTML = name_in;
   document.getElementById("date-out").innerHTML = date_in;
-
+  if (number_in_1 < 0) {
+    number_in_1 = 0;
+  }
+  if (number_in_2 < 0) {
+    number_in_2 = 0;
+  }
+  if (number_in_3 < 0) {
+    number_in_3 = 0;
+  }
   document.getElementById("so-luong-1").innerHTML = number_in_1;
   document.getElementById("so-luong-2").innerHTML = number_in_2;
   document.getElementById("so-luong-3").innerHTML = number_in_3;
+
   let tien_1 = number_in_1 * 1;
   let tien_2 = number_in_2 * 2;
   let tien_3 = number_in_3 * 3;
